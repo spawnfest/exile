2e9fe436c1fa6a69a79d49a5ffe511303d24d545
Evadne Wu <ev@radi.ws>
2019-09-22 21:05:13 +0100

    Refined project; added Deployment support; removed unused code
    - Removed Exile Auth from tree
    - Removed Ecto SQL
    - Removed Postgrex
    - Refined existing files and removed duplicative comments

--- config/config.exs
+++ config/config.exs
@@ -1,27 +1,8 @@
-# This file is responsible for configuring your umbrella
-# and **all applications** and their dependencies with the
-# help of Mix.Config.
-#
-# Note that all applications in your umbrella share the
-# same configuration and dependencies, which is why they
-# all use the same configuration file. If you want different
-# configurations or dependencies per app, it is best to
-# move said applications out of the umbrella.
 use Mix.Config
 
-# Configure Mix tasks and generators
-config :exile,
-  ecto_repos: [Exile.Repo]
-
 config :exile_web,
-  ecto_repos: [Exile.Repo],
   generators: [context_app: :exile]
 
-config :exile_auth,
-  ecto_repos: [ExileAuth.Repo],
-  generators: [context_app: :exile_auth]
-
-# Configures the endpoint
 config :exile_web, ExileWeb.Endpoint,
   url: [host: "localhost"],
   live_view: [signing_salt: "IG9Dv+0Y"],
@@ -29,19 +10,11 @@ config :exile_web, ExileWeb.Endpoint,
   render_errors: [view: ExileWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: ExileWeb.PubSub, adapter: Phoenix.PubSub.PG2]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
-
-# Configure Guardian secret for generating JWTs
-config :exile_auth, ExileAuth.Guardian,
-  issuer: "exile_auth",
-  secret_key: "yYSAJxjltNf3Acl0FjsDjD0yp0X5kLGev4z2eTR2TLaKoURCJsdowKSVbniohREn"
-
 import_config "#{Mix.env()}.exs"
 
 if File.exists?(Path.join([__DIR__, "#{Mix.env()}.secret.exs"])) do
--- mix.exs
+++ mix.exs
@@ -5,23 +5,26 @@ defmodule Exile.Umbrella.MixProject do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: releases()
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options.
-  #
-  # Dependencies listed here are available only for this project
-  # and cannot be accessed from applications inside the apps folder
   defp deps do
-    [{:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false}]
+    [
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false}
+    ]
+  end
+  
+  defp releases do
+    [
+      exile: [
+        applications: [
+          exile: :permanent,
+          exile_web: :permanent
+        ],
+        version: "0.1.0"
+      ]
+    ]
   end
 end
