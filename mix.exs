7bab89e85264c1cc749e600b744f56dd73c9fdbf
Steven Holdsworth <sholdsworth@gmail.com>
2019-09-22 22:31:39 +0100

    Added docs generation and README

--- mix.exs
+++ mix.exs
@@ -6,16 +6,21 @@ defmodule Exile.Umbrella.MixProject do
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      releases: releases()
+      releases: releases(),
+      name: "Exile",
+      source_url: "https://github.com/spawnfest/doodler",
+      homepage_url: "https://exile-web.gigalixirapp.com",
+      docs: docs()
     ]
   end
 
   defp deps do
     [
-      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.21.2", only: [:dev], runtime: false}
     ]
   end
-  
+
   defp releases do
     [
       exile: [
@@ -25,6 +30,13 @@ defmodule Exile.Umbrella.MixProject do
         ],
         version: "0.1.0"
       ]
+    ]
+  end
+
+  defp docs do
+    [
+      logo: "priv/static/exile.svg",
+      extras: ["README.md"]
     ]
   end
 end
